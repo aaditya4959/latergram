59db245358aa4b35e4c12e5fd83cc7ae44a68f51
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-01-06 23:28:55 +0530

    Minor Changes in Endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express from "express";  // This statement is possible due to typescript.
 import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
 import {UserModel} from "./models/users";
 
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json())
+
 
 app.post("/api/v1/signup", async (req,res) => {
     console.log("Signup request received");
@@ -21,6 +23,38 @@ app.post("/api/v1/signup", async (req,res) => {
 
 // Signin request (User will be assigned the jwt in this step and will be stored in the browser only)
 app.post("api/v1/signin", async (req , res) =>{
+
+});
+
+
+
+// content posting endpoint ( again the use of jwt)
+app.post("api/v1/content" , async (req,res) => {
+
+});
+
+
+
+
+// get on content
+app.get("api/v1/content", async (req , res) => {
+
+});
+
+
+//delete on content
+app.delete("api/v1/content:id", async (req, res) => {
+
+});
+
+// sharable
+app.post("api/v1/brain/share" , async (req , res) => {
+
+});
+
+
+// Getting the content of the shared link
+app.get("api/v1/brain/share:link", async (req , res) => {
 
 });
 
