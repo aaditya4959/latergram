5a7f6eab5819f16a11715fcfe2d9f6981b81bffe
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-01-07 12:26:03 +0530

    mongo uri made private

--- backend/src/Controller/dbConnection.ts
+++ backend/src/Controller/dbConnection.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const mongoURI = process.env.MONGO_URI || "";
 
-// "mongodb+srv://aadibirsingh:TnA5DETQdeg3IejG@cluster0.wwvkh.mongodb.net/latergram"
+
 
 
 export  const db = async () => {
