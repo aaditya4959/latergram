776b47a8ea8751ed76b52d8f5884e5f796fb695a
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-07-23 21:18:59 +0530

    Custom Test Case Added

--- backend/__tests__/dummy.test.js
+++ backend/__tests__/dummy.test.js
@@ -1,3 +1,20 @@
+import axios from 'axios';
+import { expect, jest } from '@jest/globals';
+import { suite } from 'node:test';
+
 test('adds 1 + 1 to equal 2', () => {
   expect(1 + 1).toBe(2);
 });
+
+
+test('Sign-Up should work in the backend' ,async () => {
+    const randomNumber = Math.floor(Math.random() * 1000);
+    const user = {
+      username : 'user' + randomNumber,
+      password : 'password' + randomNumber,
+    }
+
+    const response = await axios.posy("http://localhost:8080/api/v1/signup", user);
+
+    expect([201, 409]).toContain(response.status);
+})
