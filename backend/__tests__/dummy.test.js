2c1bb11f4a304275d9f77f1f37684bd045a406f4
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-07-23 21:35:58 +0530

    Reverted

--- backend/__tests__/dummy.test.js
+++ backend/__tests__/dummy.test.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+// const axios = require('axios');
 
 
 test('adds 1 + 1 to equal 2', () => {
@@ -6,14 +6,14 @@ test('adds 1 + 1 to equal 2', () => {
 });
 
 
-test('Sign-Up should work in the backend' ,async () => {
-    const randomNumber = Math.floor(Math.random() * 1000);
-    const user = {
-      username : 'user' + randomNumber,
-      password : 'password' + randomNumber,
-    }
+// test('Sign-Up should work in the backend' ,async () => {
+//     const randomNumber = Math.floor(Math.random() * 1000);
+//     const user = {
+//       username : 'user' + randomNumber,
+//       password : 'password' + randomNumber,
+//     }
 
-    const response = await axios.post("http://localhost:8080/api/v1/signup", user);
+//     const response = await axios.post("http://localhost:8080/api/v1/signup", user);
 
-    expect([201, 409]).toContain(response.status);
-})
+//     expect([201, 409]).toContain(response.status);
+// })
